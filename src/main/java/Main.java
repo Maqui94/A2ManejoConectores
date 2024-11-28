21be9719b98190799e3dfa0b939ccbe88951cff2
Maqui94 <161259928+Maqui94@users.noreply.github.com>
2024-11-28 12:15:27 +0100

    fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Concesionario2 concesionario = new Concesionario2();
 
-      //  concesionario.menuCoche();
-      //  concesionario.agregarPasajero();
-      //  concesionario.eliminarPasajero();
-     //   concesionario.consultarPasajero();
-      //  concesionario.mostrarPasajerosTotales();
-       // concesionario.agregarPasajeroACoche();
-     //   concesionario.eliminarPasajeroCoche();
-     //  concesionario.mostrarPasajerosEnCoche();
         concesionario.menuPrincipal();
+
     }
 }
